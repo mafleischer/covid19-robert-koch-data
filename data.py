4e1c47ebe85fcadf721f2fd9025c9e091c55ad53
ullu <ullu@ullu.de>
2020-03-26 03:13:32 +0100

    Write stuff for per German state cases
    Also modify .gitignore

--- data.py
+++ data.py
@@ -9,6 +9,15 @@ import seaborn as sns
 sns.set()
 %matplotlib inline
 
+# daily infections in Germany
 url_daily_corona = "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
 
-urllib.request.urlretrieve(url_daily_corona)
+# infections per 100k residents in German states
+url_corona_bl_100k = "https://opendata.arcgis.com/datasets/ef4b445a53c1406892257fe63129a8ea_0.csv"
+
+fname1 = "./RKI_covid19_daily_de.csv"
+fname2 = "./RKI_covid19_states_100k.csv"
+urllib.request.urlretrieve(url_daily_corona, fname1)
+urllib.request.urlretrieve(url_daily_corona, fname2)
+
+df_daily_data = pd.read_csv(fname1)
